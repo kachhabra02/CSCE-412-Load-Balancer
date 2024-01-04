bb6294cbb3108c0292c57006e7174fc52554580e
Krish Chhabra <kachhabra02@tamu.edu>
2024-01-04 23:23:08 +0800

    Added implementation for RequestGenerator

--- RequestGenerator.cpp
+++ RequestGenerator.cpp
@@ -1,1 +1,51 @@
 #include "RequestGenerator.h"
+
+string RequestGenerator::generateIP() {
+    return "" + to_string(rand() % 256) + "." + to_string(rand() % 256) + "." + to_string(rand() % 256) + "." + to_string(rand() % 256);
+}
+
+RequestGenerator::RequestGenerator() : reqTimeMin(4), reqTimeMax(100), reqProb(5) {
+    srand(time(0));
+}
+
+RequestGenerator::RequestGenerator(int prob) : reqTimeMin(4), reqTimeMax(100), reqProb(prob) {
+    srand(time(0));
+}
+
+RequestGenerator::RequestGenerator(int timeMin, int timeMax) : reqTimeMin(timeMin), reqTimeMax(timeMax), reqProb(5) {
+    srand(time(0));
+}
+
+RequestGenerator::RequestGenerator(int timeMin, int timeMax, int prob) : reqTimeMin(timeMin), reqTimeMax(timeMax), reqProb(prob) {
+    srand(time(0));
+}
+
+void RequestGenerator::setRequestProbability(int prob) {
+    reqProb = prob;
+}
+
+vector<Request*> RequestGenerator::getRequests() {
+    vector<Request*> requests;
+
+    while ((rand() % reqProb) == 0) {
+        requests.push_back(new Request());
+        requests.back()->srcIP = generateIP();
+        requests.back()->dstIP = generateIP();
+        requests.back()->time = rand() % (reqTimeMax - reqTimeMin + 1) + reqTimeMin;
+    }
+
+    return requests;
+}
+
+vector<Request*> RequestGenerator::getRequests(int numRequests) {
+    vector<Request*> requests;
+
+    for (int i = 0; i < numRequests; ++i) {
+        requests.push_back(new Request());
+        requests.back()->srcIP = generateIP();
+        requests.back()->dstIP = generateIP();
+        requests.back()->time = rand() % (reqTimeMax - reqTimeMin + 1) + reqTimeMin;
+    }
+
+    return requests;
+}
--- RequestGenerator.h
+++ RequestGenerator.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include "Request.h"
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+using std::string, std::to_string, std::vector;
+
 class RequestGenerator {
 private:
-    // TODO
+    int reqTimeMin, reqTimeMax;
+    int reqProb;
+
+    string generateIP();
+
 public:
-    // TODO
+    RequestGenerator();
+    RequestGenerator(int prob);
+    RequestGenerator(int timeMin, int timeMax);
+    RequestGenerator(int timeMin, int timeMax, int prob);
+
+    void setRequestProbability(int prob);
+
+    vector<Request*> getRequests();
+    vector<Request*> getRequests(int numRequests);
 };
